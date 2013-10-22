82a698cb66892cae27be323d9483e1311b54e514
Jacob Nielsen <indkasse@gmail.com>
2013-10-22 09:55:02 +0200

    alignment issue

--- SampleCode/GettingStarted/GameAnalytics.lua
+++ SampleCode/GettingStarted/GameAnalytics.lua
@@ -27,25 +27,24 @@ GameAnalytics.batchRequests             = false
 GameAnalytics.batchRequestsInterval     = 30	-- seconds (minimum 1 second)
 
 -- Quality
-
 GameAnalytics.submitSystemInfo          = false
 GameAnalytics.submitUnhandledErrors     = false
 GameAnalytics.submitStackTraces         = false
 GameAnalytics.submitMemoryWarnings      = false -- iOS only!
-GameAnalytics.maxErrorCount				= 20    -- errors per session
+GameAnalytics.maxErrorCount             = 20    -- errors per session
 
 -- Design
-GameAnalytics.useStoryboard				= false
+GameAnalytics.useStoryboard             = false
 GameAnalytics.submitStoryboardEvents    = false
 
-GameAnalytics.submitAverageFps			= false
-GameAnalytics.submitAverageFpsInterval	= 30 	-- seconds (minimum 5)
+GameAnalytics.submitAverageFps          = false
+GameAnalytics.submitAverageFpsInterval  = 30 	-- seconds (minimum 5)
 
 GameAnalytics.submitCriticalFps         = false
 GameAnalytics.submitCriticalFpsInterval = 5 	-- seconds (minimum 5)
-GameAnalytics.criticalFpsRange			= 15	-- frames  (minimum 10)
+GameAnalytics.criticalFpsRange          = 15	-- frames  (minimum 10)
 
-GameAnalytics.criticalFpsBelow 			= display.fps/2
+GameAnalytics.criticalFpsBelow          = display.fps/2
 
 -----------------------------------------------
 
