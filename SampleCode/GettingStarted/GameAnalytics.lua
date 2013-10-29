b64eeec8084484a9122e1932b417ff120ea904b1
Jacob Nielsen <indkasse@gmail.com>
2013-10-29 17:20:28 +0100

    Update GameAnalytics.lua

--- SampleCode/GettingStarted/GameAnalytics.lua
+++ SampleCode/GettingStarted/GameAnalytics.lua
@@ -20,11 +20,11 @@ GameAnalytics.isDebug                   = true
 GameAnalytics.runInSimulator            = true
 GameAnalytics.submitWhileRoaming        = false
 GameAnalytics.archiveEvents             = true
-GameAnalytics.archiveEventsLimit        = 512 	-- kilobytes 
+GameAnalytics.archiveEventsLimit        = 512   -- kilobytes 
 GameAnalytics.waitForCustomUserID       = false
 GameAnalytics.newSessionOnResume        = false
 GameAnalytics.batchRequests             = false
-GameAnalytics.batchRequestsInterval     = 30	-- seconds (minimum 1 second)
+GameAnalytics.batchRequestsInterval     = 30    -- seconds (minimum 1 second)
 
 -- Quality
 GameAnalytics.submitSystemInfo          = false
@@ -38,11 +38,11 @@ GameAnalytics.useStoryboard             = false
 GameAnalytics.submitStoryboardEvents    = false
 
 GameAnalytics.submitAverageFps          = false
-GameAnalytics.submitAverageFpsInterval  = 30 	-- seconds (minimum 5)
+GameAnalytics.submitAverageFpsInterval  = 30    -- seconds (minimum 5)
 
 GameAnalytics.submitCriticalFps         = false
-GameAnalytics.submitCriticalFpsInterval = 5 	-- seconds (minimum 5)
-GameAnalytics.criticalFpsRange          = 15	-- frames  (minimum 10)
+GameAnalytics.submitCriticalFpsInterval = 5     -- seconds (minimum 5)
+GameAnalytics.criticalFpsRange          = 15    -- frames  (minimum 10)
 
 GameAnalytics.criticalFpsBelow          = display.fps/2
 
